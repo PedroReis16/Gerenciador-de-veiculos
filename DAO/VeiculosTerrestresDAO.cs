748cf395f414a72b3af098eb48e7012ffae8f38e
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2023-05-06 22:34:24 -0300

    Adicao de todas as classes que serão utilizadas para CRUD no projeto

--- DAO/VeiculosTerrestresDAO.cs
+++ DAO/VeiculosTerrestresDAO.cs
@@ -8,6 +8,17 @@ namespace Gerenciador_de_veículos.DAO
 {
     public class VeiculosTerrestresDAO
     {
-        
+        /*
+        Método para os carros
+        Método para as motos
+        Método para os ônibus
+        Método para os caminhões
+        Método para todos juntos
+
+        Todos esses métodos, precisam salvar e retornar os dados sobre os veículos terrestres em questão, menos o método para todos juntos, que é apenas um método de leitura.
+         */
+
+
+
     }
 }
