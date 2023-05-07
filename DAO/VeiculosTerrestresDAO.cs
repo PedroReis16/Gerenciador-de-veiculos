03de252242ade3e36c4c8fa62b25af4c1beadf9b
PedroReis16 <pedro_henrique_dsr@hotmail.com>
2023-05-07 10:48:29 -0300

    Conclusão da classe DAO que busca todos os veículos terrestres

--- DAO/VeiculosTerrestresDAO.cs
+++ DAO/VeiculosTerrestresDAO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Gerenciador_de_veículos.Interface;
+using Gerenciador_de_veículos.Objects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +10,22 @@ namespace Gerenciador_de_veículos.DAO
 {
     public class VeiculosTerrestresDAO
     {
-        /*
-        Método para os carros
-        Método para as motos
-        Método para os ônibus
-        Método para os caminhões
-        Método para todos juntos
+        public List<IVeiculos> GetAllVehicles()
+        {
+            List<IVeiculos> list = new List<IVeiculos>();
 
-        Todos esses métodos, precisam salvar e retornar os dados sobre os veículos terrestres em questão, menos o método para todos juntos, que é apenas um método de leitura.
-         */
+            List<Carro> carros = new List<Carro>();
+            List<Moto> motos = new List<Moto>();
+            List<Onibus> onibus = new List<Onibus>();
+            List<Caminhao> caminhaos= new List<Caminhao>();
+
+            list.AddRange(carros);
+            list.AddRange(motos);
+            list.AddRange(onibus);
+            list.AddRange(caminhaos);
+
+            return list;
+        }
 
 
 
